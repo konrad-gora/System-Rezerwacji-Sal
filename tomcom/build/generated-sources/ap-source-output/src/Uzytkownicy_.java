fd84571437743072c14dd1f848f291e3ec388b15
bartoszholko <bartoszholko@gmail.com>
2015-03-05 21:30:56 +0100

    Pandel administracyjny do contentu part I

---
+++ tomcom/build/generated-sources/ap-source-output/src/Uzytkownicy_.java
@@ -0,0 +1,19 @@
+package src;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-03-05T21:15:15")
+@StaticMetamodel(Uzytkownicy.class)
+public class Uzytkownicy_ { 
+
+    public static volatile SingularAttribute<Uzytkownicy, String> imie;
+    public static volatile SingularAttribute<Uzytkownicy, String> nazwisko;
+    public static volatile SingularAttribute<Uzytkownicy, String> haslo;
+    public static volatile SingularAttribute<Uzytkownicy, Integer> id;
+    public static volatile SingularAttribute<Uzytkownicy, String> login;
+    public static volatile SingularAttribute<Uzytkownicy, String> pesel;
+    public static volatile SingularAttribute<Uzytkownicy, String> email;
+
+}
